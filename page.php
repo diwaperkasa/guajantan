258f94f8156ef344c1b246744b2bdae70f4b5864
diwaperkasa <diwa.perkasa@gmail.com>
2026-08-11 16:24:37 +0700

    init

--- page.php
+++ page.php
@@ -12,10 +12,10 @@ function content()
         <?php while (have_posts()) : the_post(); ?>
             <article <?php post_class(); ?>>
                 <header class="py-4">
-                    <h1 class="page__title kepler fw-bold text-center"><?php the_title(); ?></h1>
+                    <h1 class="page__title playfair-font fw-bold text-center"><?php the_title(); ?></h1>
                 </header>
 
-                <div class="post__content karla">
+                <div class="post__content">
                     <?php the_content(); ?>
                 </div>
             </article>
