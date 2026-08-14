037e53061dace68ef68e04187bd1bfaed3676637
diwaperkasa <diwa.perkasa@gmail.com>
2026-08-14 20:08:45 +0700

    init

--- components/header.php
+++ components/header.php
@@ -84,7 +84,7 @@
                     </button>
                 </li>
                 <li class="nav-item d-none d-lg-inline">
-                    <button class="navbar-brand btn p-0 fs-6 d-flex align-items-center" data-bs-toggle="offcanvas" href="#offcanvasMenu" aria-controls="offcanvasMenu">
+                    <button class="navbar-brand btn p-0 fs-6 d-flex align-items-center" data-bs-toggle="offcanvas" href="#offcanvasSearch" aria-controls="offcanvasSearch">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                         </svg>
